21e1d6bc3a404e7d53b192f39c09c2c54107bd2a
Alexander Matroskin <cqb.matroskin@gmail.com>
2017-02-13 23:49:30 +0300

    [module5-task1] Работа с картой пинов выведена в отдельный модуль

--- js/form.js
+++ js/form.js
@@ -1,8 +1,5 @@
 'use strict';
 
-var tokyoMapElement = document.querySelector('.tokyo__pin-map');
-var dialogElement = document.querySelector('.dialog');
-var closeElement = dialogElement.querySelector('.dialog__close');
 var formElement = document.querySelector('.notice__form');
 var inputTitleElement = formElement.querySelector('#title');
 var inputAddressElement = formElement.querySelector('#address');
@@ -12,11 +9,7 @@ var capacitySelectElement = formElement.querySelector('#capacity');
 var timeInSelectElement = formElement.querySelector('#time');
 var timeOutSelectElement = formElement.querySelector('#timeout');
 var typeSelectElement = formElement.querySelector('#type');
-var pinActive = tokyoMapElement.querySelector('.pin--active');
 
-var EventType = {
-  CLICK: 'click'
-};
 
 /** массив объектов с парметрами для полей */
 var config = [
@@ -71,16 +64,6 @@ var SelectConfig = {
   }
 };
 
-var ClassName = {
-  INVISIBLE: 'invisible',
-  PIN_ACTIVE: 'pin--active',
-  PIN: 'pin'
-};
-
-var KeyCode = {
-  ESCAPE: 27,
-  ENTER: 13
-};
 
 /* добавляет аттрибуты всем полям заданным в массиве */
 function arrToValidate(arrConfig) {
@@ -94,76 +77,6 @@ function arrToValidate(arrConfig) {
         fieldElement[key] = elementAttr[key];
       }
     }
-  }
-}
-
-/* проверяет совпадает ли evt.keyCode с клавишей Enter */
-function pinActivate(evt) {
-  return evt.keyCode === KeyCode.ENTER || evt.type === EventType.CLICK;
-}
-
-/* вызывает функцию активации по нажатию или клику на .pin */
-function pinHandler(evt) {
-  if (pinActivate(evt)) {
-    selectPin(getClosestElement(evt.target, '.' + ClassName.PIN));
-  }
-}
-
-/*
- * если переданный элемент не соответствует классу,
- * поднимаемся к родителю и проверяем его
- */
-function getClosestElement(element, className) {
-  while (element) {
-    if (element.matches(className)) {
-      return element;
-    } else {
-      element = element.parentElement;
-    }
-  }
-  return null;
-}
-
-/*
- * удаляет класс у неактивного элемента
- * добаляет класс target-у
- * показывает диалоговое окно
- */
-function selectPin(target) {
-  if (target.classList.contains(ClassName.PIN_ACTIVE)) {
-    return;
-  } else if (pinActive) {
-    pinActive.classList.remove(ClassName.PIN_ACTIVE);
-    pinActive.setAttribute('aria-checked', false);
-  }
-  pinActive = target;
-  pinActive.classList.add(ClassName.PIN_ACTIVE);
-  pinActive.setAttribute('aria-checked', true);
-  toggleDialog(true);
-}
-
-function toggleDialog(flag) {
-  dialogElement.classList.toggle(ClassName.INVISIBLE, !flag);
-  dialogElement.setAttribute('aria-hidden', !flag);
-  if (flag) {
-    closeElement.addEventListener('click', closeDialogHandler);
-    document.addEventListener('keydown', keyDownToCloseDialog);
-  } else {
-    closeElement.removeEventListener('click', closeDialogHandler);
-    document.removeEventListener('keydown', keyDownToCloseDialog);
-  }
-}
-
-/* закрывает диалоговое окно по клику */
-function closeDialogHandler(evt) {
-  evt.preventDefault();
-  toggleDialog(false);
-}
-
-/* закрывает диалоговое окно при нажатии Esc */
-function keyDownToCloseDialog(evt) {
-  if (evt.keyCode === KeyCode.ESCAPE) {
-    toggleDialog(false);
   }
 }
 
@@ -225,8 +138,6 @@ arrToValidate(config);
 changeSelectCapacityHandler();
 changeTypeSelectHandler();
 validateInputTitleHandler();
-tokyoMapElement.addEventListener('click', pinHandler);
-tokyoMapElement.addEventListener('keydown', pinHandler);
 timeInSelectElement.addEventListener('change', synchronizeSelectTimeHandler);
 timeOutSelectElement.addEventListener('change', synchronizeSelectTimeHandler);
 roomSelectElement.addEventListener('change', changeSelectCapacityHandler);
